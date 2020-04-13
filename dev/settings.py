fa89704287a0bceaf6027728c7eb1dbce60a6a2b
iforvard <48457650+iforvard@users.noreply.github.com>
2020-04-14 00:02:25 +0300

    add bootstrap_pagination

--- dev/settings.py
+++ dev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.vk',
     'bootstrap4',
+    'bootstrap_pagination',
 ]
 SITE_ID = 1
 
