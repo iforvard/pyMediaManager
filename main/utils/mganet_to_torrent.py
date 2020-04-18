509a2a38e0a4e36e6381248466759d9c86dab1e9
iforvard <48457650+iforvard@users.noreply.github.com>
2020-04-18 09:57:16 +0300

    fix and upd

--- main/utils/mganet_to_torrent.py
+++ main/utils/mganet_to_torrent.py
@@ -4,15 +4,13 @@ from magnet2torrent import Magnet2Torrent, FailedToFetchException
 
 
 class M2T(Magnet2Torrent):
-    @property
-    def name(self):
-        return (
-            self._parse_url()[2].decode()
-                .strip(".")
-                .replace("/", "")
-                .replace("\\", "")
-                .replace(":", "")
-        )
+    def _parse_url(self):
+        infohash, trackers, name = super(M2T, self)._parse_url()
+        try:
+            name = name.decode()
+        except AttributeError:
+            pass
+        return infohash, trackers, name
 
 
 async def fetch_that_torrent(magnet_url):
@@ -26,9 +24,11 @@ async def fetch_that_torrent(magnet_url):
 
 def get_torrent_by_magnet(magnet_url):
     filename, torrent_data = asyncio.run(fetch_that_torrent(magnet_url))
+    print(filename)
     return [torrent_data]
 
 
 if __name__ == '__main__':
-    url = 'magnet:?xt=urn:btih:9A73039A6B17F27FFE33D5387E79F5A6137645F5&tr=http%3A%2F%2Fbt3.t-ru.org%2Fann%3Fmagnet'
-    get_torrent_by_magnet(url)
+    url2 = 'magnet:?xt=urn:btih:0CC4F994665E9CA6CBFD9DD794C60BE684FF2175&tr=http%3A%2F%2Fbt.t-ru.org%2Fann%3Fmagnet&dn=%D0%A0%D0%B8%D0%BA%D0%BE%D1%88%D0%B5%D1%82%20%2F%20%D0%A1%D0%B5%D1%80%D0%B8%D0%B8%3A%201-16%20%D0%B8%D0%B7%2016%20(%D0%94%D0%B5%D0%BD%D0%B8%D1%81%20%D0%9A%D0%B0%D1%80%D1%8B%D1%88%D0%B5%D0%B2%2C%20%D0%92%D1%8F%D1%87%D0%B5%D1%81%D0%BB%D0%B0%D0%B2%20%D0%9A%D0%B8%D1%80%D0%B8%D0%BB%D0%BB%D0%BE%D0%B2)%20%5B2020%2C%20%D0%B4%D0%B5%D1%82%D0%B5%D0%BA%D1%82%D0%B8%D0%B2%2C%20%D0%BA%D1%80%D0%B8%D0%BC%D0%B8%D0%BD%D0%B0%D0%BB%2C%20%D0%B1%D0%BE%D0%B5%D0%B2%D0%B8%D0%BA%2C%20SATRip-AVC%5D'
+    url = 'magnet:?xt=urn:btih:108F8A6F1F72C30E56F5259CF28ECCEA1FE76812&tr=http%3A%2F%2Fbt4.t-ru.org%2Fann%3Fmagnet'
+    print(get_torrent_by_magnet(url2))
