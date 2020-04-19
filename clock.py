72eaeb6f3b27ccdc73d4b531454fe2800fe01849
iforvard <48457650+iforvard@users.noreply.github.com>
2020-04-19 14:53:17 +0300

    fix and upd

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ sched = BlockingScheduler()
 # Интервал можно указать с разными параметрами:
 # https://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html
 # https://apscheduler.readthedocs.io/en/stable/modules/triggers/interval.html
-@sched.scheduled_job('interval', minutes=sched_time_min, seconds=sched_time_sec)
+@sched.scheduled_job('interval', seconds=sched_time_sec)
 def timed_job():
     # iforvard - нужно заменить на логин пользователя в системе,
     # Если после проверки обновлений требуется загрузка в bashCommand нужно добавить -dw.
