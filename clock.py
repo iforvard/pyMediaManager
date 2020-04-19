240a2f1773e2367ef8ec40e5f0270549e42b195a
iforvard <48457650+iforvard@users.noreply.github.com>
2020-04-19 14:40:56 +0300

    fix and upd

--- clock.py
+++ clock.py
@@ -1,13 +1,21 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 import subprocess
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched_time_min = os.environ.get('SCHED_TIME_MIN', '*')
+sched_time_sec = os.environ.get('SCHED_TIME_MIN', '10')
 
 sched = BlockingScheduler()
 
-# Интервал можно указать разными параметрами см. https://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html
-@sched.scheduled_job('interval', days=1)
+
+# Интервал можно указать с разными параметрами:
+# https://apscheduler.readthedocs.io/en/latest/modules/triggers/cron.html
+# https://apscheduler.readthedocs.io/en/stable/modules/triggers/interval.html
+@sched.scheduled_job('interval', minutes=sched_time_min, seconds=sched_time_sec)
 def timed_job():
     # iforvard - нужно заменить на логин пользователя в системе,
-    # Если после проверки обновлений требуется загрузка нужно добавить -dw.
+    # Если после проверки обновлений требуется загрузка в bashCommand нужно добавить -dw.
     bashCommand = "python3 manage.py check_torrents iforvard"
     subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     print(f'Выполненна команда {bashCommand}')
