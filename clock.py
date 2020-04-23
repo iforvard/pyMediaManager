a4a05553b4b3d00d0a6d41b2350e5642e7b7abd3
iforvard <48457650+iforvard@users.noreply.github.com>
2020-04-23 03:29:58 +0300

    fix and upd

--- clock.py
+++ clock.py
@@ -25,8 +25,8 @@ def timed_job():
     bashCommand = "python3 manage.py check_torrents iforvard"
     subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     print(f'Выполненна команда {bashCommand}')
-    sched_next_time = datetime.now() + timedelta(minutes=sched_time_min)
-    print(f'Следущуая итерация через {sched_time_min} минут, в {sched_next_time}')
+    sched_next_time = datetime.now() + timedelta(minutes=sched_time_hour)
+    print(f'Следущуая итерация через {sched_time_hour} минут, в {sched_next_time}')
 
 
 sched.start()
